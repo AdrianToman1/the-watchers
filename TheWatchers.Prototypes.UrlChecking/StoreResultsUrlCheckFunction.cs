a056ab922903499f0a2fc2cccabb2de62e132fd0
Adrian Toman <adrian@adriantoman.com>
2021-05-28 20:56:35 +1000

    Implement saving result to Cosmos DB

--- TheWatchers.Prototypes.UrlChecking/StoreResultsUrlCheckFunction.cs
+++ TheWatchers.Prototypes.UrlChecking/StoreResultsUrlCheckFunction.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -18,40 +19,64 @@ namespace TheWatchers.Prototypes.UrlChecking
         private static readonly string PrimaryKey =
             "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-        // The Cosmos client instance
-        private static CosmosClient cosmosClient;
-
-        // The database we will create
-        private static Database database;
-
-        // The container we will create.
-        private static Container container;
-
         // The name of the database and container we will create
         private static string databaseId = "FamilyDatabase";
-        private static string containerId = "FamilyContainer";
-
+        private static string containerId = "ResultContainer";
 
         private static readonly HttpClient Client = new HttpClient();
 
         [FunctionName("StoreResultsUrlCheckFunction")]
         public static async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
+            var executedAt = DateTimeOffset.Now;
+
             const string url = "https://adriantoman.com/";
+
+            var requestSentAt = DateTimeOffset.Now;
 
             var response = await Client.GetAsync(url);
 
+            var responseReceivedAt = DateTimeOffset.Now;
+
             log.LogInformation($"{DateTime.Now} {url} {response.StatusCode}");
+
+            var urlCheckResult = new UrlCheckResult
+            {
+                Id = Guid.NewGuid().ToString(),
+                ExecutedAt = executedAt,
+                ScheduledFor = myTimer.ScheduleStatus.Next,
+                RequestSentAt = requestSentAt,
+                ResponseSentAt = response.Headers.Date,
+                ResponseReceivedAt = responseReceivedAt,
+                StatusCode = new HttpStatusCode
+                {
+                    StatusCode = (int)response.StatusCode,
+                    ReasonPhrase = response.StatusCode.ToString()
+                },
+                Headers = response.Headers.ToList().ConvertAll(i => new Tuple<string, IEnumerable<string>>(i.Key, i.Value)),
+                Url = url
+            };
+
+            urlCheckResult.Headers.AddRange(response.Content.Headers.ToList().ConvertAll(i => new Tuple<string, IEnumerable<string>>(i.Key, i.Value)));
+
+            var s = await response.Content.ReadAsStringAsync();
+
+            urlCheckResult.Body = s.Substring(0, 1000);
 
             try
             {
-                Console.WriteLine("Beginning operations...\n");
-                await GetStartedDemoAsync();
+                var container = await GetDatabaseContainer();
+
+                var andersenFamilyResponse =
+                    await container.CreateItemAsync(urlCheckResult, new PartitionKey(urlCheckResult.Url));
+
+                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
+                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", andersenFamilyResponse.Resource.Id, andersenFamilyResponse.RequestCharge);
+
 
             }
             catch (CosmosException de)
             {
-                Exception baseException = de.GetBaseException();
                 Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
             }
             catch (Exception e)
@@ -60,187 +85,38 @@ namespace TheWatchers.Prototypes.UrlChecking
             }
         }
 
-        public static async Task GetStartedDemoAsync()
+        public static async Task<Container> GetDatabaseContainer()
         {
-            // Create a new instance of the Cosmos Client
-            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
-            await CreateDatabaseAsync();
-            await CreateContainerAsync();
-            await AddItemsToContainerAsync();
-        }
-
-        /// <summary>
-        /// Create the database if it does not exist
-        /// </summary>
-        private static async Task CreateDatabaseAsync()
-        {
-            // Create a new database
-            database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
-            Console.WriteLine("Created Database: {0}\n", database.Id);
-        }
-
-        /// <summary>
-        /// Create the container if it does not exist. 
-        /// Specifiy "/LastName" as the partition key since we're storing family information, to ensure good distribution of requests and storage.
-        /// </summary>
-        /// <returns></returns>
-        private static async Task CreateContainerAsync()
-        {
-            // Create a new container
-            container = await database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
-            Console.WriteLine("Created Container: {0}\n", container.Id);
-        }
-
-
-        /// <summary>
-        /// Add Family items to the container
-        /// </summary>
-        private static async Task AddItemsToContainerAsync()
-        {
-            // Create a family object for the Andersen family
-            Family andersenFamily = new Family
-            {
-                Id = "Andersen.1",
-                LastName = "Andersen",
-                Parents = new Parent[]
-                {
-                    new Parent {FirstName = "Thomas"},
-                    new Parent {FirstName = "Mary Kay"}
-                },
-                Children = new Child[]
-                {
-                    new Child
-                    {
-                        FirstName = "Henriette Thaulow",
-                        Gender = "female",
-                        Grade = 5,
-                        Pets = new Pet[]
-                        {
-                            new Pet {GivenName = "Fluffy"}
-                        }
-                    }
-                },
-                Address = new Address { State = "WA", County = "King", City = "Seattle" },
-                IsRegistered = false
-            };
-
-            try
-            {
-                // Read the item to see if it exists.  
-                ItemResponse<Family> andersenFamilyResponse =
-                    await container.ReadItemAsync<Family>(andersenFamily.Id, new PartitionKey(andersenFamily.LastName));
-                Console.WriteLine("Item in database with id: {0} already exists\n", andersenFamilyResponse.Resource.Id);
-            }
-            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-            {
-                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
-                ItemResponse<Family> andersenFamilyResponse =
-                    await container.CreateItemAsync<Family>(andersenFamily, new PartitionKey(andersenFamily.LastName));
-
-                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n",
-                    andersenFamilyResponse.Resource.Id, andersenFamilyResponse.RequestCharge);
-            }
-
-            // Create a family object for the Wakefield family
-            Family wakefieldFamily = new Family
-            {
-                Id = "Wakefield.7",
-                LastName = "Wakefield",
-                Parents = new Parent[]
-                {
-                    new Parent {FamilyName = "Wakefield", FirstName = "Robin"},
-                    new Parent {FamilyName = "Miller", FirstName = "Ben"}
-                },
-                Children = new Child[]
-                {
-                    new Child
-                    {
-                        FamilyName = "Merriam",
-                        FirstName = "Jesse",
-                        Gender = "female",
-                        Grade = 8,
-                        Pets = new Pet[]
-                        {
-                            new Pet {GivenName = "Goofy"},
-                            new Pet {GivenName = "Shadow"}
-                        }
-                    },
-                    new Child
-                    {
-                        FamilyName = "Miller",
-                        FirstName = "Lisa",
-                        Gender = "female",
-                        Grade = 1
-                    }
-                },
-                Address = new Address { State = "NY", County = "Manhattan", City = "NY" },
-                IsRegistered = true
-            };
-
-            try
-            {
-                // Read the item to see if it exists
-                ItemResponse<Family> wakefieldFamilyResponse =
-                    await container.ReadItemAsync<Family>(wakefieldFamily.Id,
-                        new PartitionKey(wakefieldFamily.LastName));
-                Console.WriteLine("Item in database with id: {0} already exists\n",
-                    wakefieldFamilyResponse.Resource.Id);
-            }
-            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-            {
-                // Create an item in the container representing the Wakefield family. Note we provide the value of the partition key for this item, which is "Wakefield"
-                ItemResponse<Family> wakefieldFamilyResponse =
-                    await container.CreateItemAsync<Family>(wakefieldFamily,
-                        new PartitionKey(wakefieldFamily.LastName));
-
-                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n",
-                    wakefieldFamilyResponse.Resource.Id, wakefieldFamilyResponse.RequestCharge);
-            }
+            var cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            var database = (await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId)).Database;
+            return (await database.CreateContainerIfNotExistsAsync(containerId, "/Url")).Container;
         }
     }
 
-
-    public class Family
+    public class UrlCheckResult
     {
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
-        public string LastName { get; set; }
-        public Parent[] Parents { get; set; }
-        public Child[] Children { get; set; }
-        public Address Address { get; set; }
-        public bool IsRegistered { get; set; }
+        public DateTimeOffset ExecutedAt { get; set; }
+        public DateTime ScheduledFor { get; set; }
+        public DateTimeOffset RequestSentAt { get; set; }
+        // May not be include in response
+        public DateTimeOffset? ResponseSentAt { get; set; }
+        public DateTimeOffset ResponseReceivedAt { get; set; }
+        public string Url { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        // Don't assume that headers are a dictionary.
+        public List<Tuple<string, IEnumerable<string>>> Headers { get; set; }
+        public string Body { get; set; }
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
     }
 
-    public class Parent
+    public class HttpStatusCode
     {
-        public string FamilyName { get; set; }
-        public string FirstName { get; set; }
-    }
-
-    public class Child
-    {
-        public string FamilyName { get; set; }
-        public string FirstName { get; set; }
-        public string Gender { get; set; }
-        public int Grade { get; set; }
-        public Pet[] Pets { get; set; }
-    }
-
-    public class Pet
-    {
-        public string GivenName { get; set; }
-    }
-
-    public class Address
-    {
-        public string State { get; set; }
-        public string County { get; set; }
-        public string City { get; set; }
+        public int StatusCode { get; set; }
+        public string ReasonPhrase { get; set; }
     }
 }
